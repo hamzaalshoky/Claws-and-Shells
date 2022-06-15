ad4b879c6505f4c8c10d280e6614ae76590c12bd
peachemce <86546508+peachemce@users.noreply.github.com>
2022-06-15 19:27:46 +0200

    Update and rename RadiationOverlay to RadiationOverlay.java

---
+++ src/main/java/net/peach/cas/client/gui/RadiationOverlay.java
@@ -0,0 +1,70 @@
+package net.peach.cas.client.gui;
+
+import org.checkerframework.checker.units.qual.h;
+
+import net.peach.cas.procedures.BlurOverlayDisplayOverlayIngameProcedure;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.Minecraft;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.platform.GlStateManager;
+
+@Mod.EventBusSubscriber({Dist.CLIENT})
+public class BlurOverlayOverlay {
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public static void eventHandler(RenderGameOverlayEvent.Pre event) {
+		if (event.getType() == RenderGameOverlayEvent.ElementType.ALL) {
+			int w = event.getWindow().getGuiScaledWidth();
+			int h = event.getWindow().getGuiScaledHeight();
+			int posX = w / 2;
+			int posY = h / 2;
+			Level _world = null;
+			double _x = 0;
+			double _y = 0;
+			double _z = 0;
+			Player entity = Minecraft.getInstance().player;
+			if (entity != null) {
+				_world = entity.level;
+				_x = entity.getX();
+				_y = entity.getY();
+				_z = entity.getZ();
+			}
+			Level world = _world;
+			double x = _x;
+			double y = _y;
+			double z = _z;
+			RenderSystem.disableDepthTest();
+			RenderSystem.depthMask(false);
+			RenderSystem.enableBlend();
+			RenderSystem.setShader(GameRenderer::getPositionTexShader);
+			RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA,
+					GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+			RenderSystem.setShaderColor(1, 1, 1, 1);
+			if (BlurOverlayDisplayOverlayIngameProcedure.execute(entity)) {
+				RenderSystem.setShaderTexture(0,
+						new ResourceLocation("cas:textures/145-1459663_blur-overlay-png-picture-freeuse-library-pattern.png"));
+				Minecraft.getInstance().gui.blit(event.getMatrixStack(), 0, 0, 0, 0, w, h, w, h);
+				RenderSystem.setShaderTexture(0,
+						new ResourceLocation("cas:textures/145-1459663_blur-overlay-png-picture-freeuse-library-pattern.png"));
+				Minecraft.getInstance().gui.blit(event.getMatrixStack(), posX + -316, posY + -145, 0, 0, 805, 347, 805, 347);
+
+			}
+			RenderSystem.depthMask(true);
+			RenderSystem.defaultBlendFunc();
+			RenderSystem.enableDepthTest();
+			RenderSystem.disableBlend();
+			RenderSystem.setShaderColor(1, 1, 1, 1);
+		}
+	}
+}
+
